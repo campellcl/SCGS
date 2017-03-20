60dbce2d460912bacd17b1319a6124f059246599
David Sawyer <sawyerda@appstate.edu>
2017-03-19 22:52:48 -0400

    Added a visual folder to store graph files, updated SCGS.py to reduce lines in the graph.dot files, and deleted unneeded visual files

--- Program/SCGS.py
+++ Program/SCGS.py
@@ -63,19 +63,23 @@ def main(tokens_file_path):
         fp.close()
     '''Build Graph Representation'''
     #Writing dot file to  graph.dot
-    with open('graph.dot', 'w') as f: 
+    with open(graph_file_path, 'w') as f: 
         # Begining of .dot file  
         f.write("digraph G {\n\n")
         for g in graph:
             #creating string for node id
             gString = tupleToStr(g)
+            iString = None
             for i in graph[g].keys():
-                iString = tupleToStr(i)
-                f.write(  gString + "  ->  " + iString +";\n") 
-        f.write("\n\n}")
+                if(iString == None):
+                    iString = tupleToStr(i)
+                iString = iString + " " + tupleToStr(i)
+            f.write(  gString + "  ->  {" + iString +"};\n") 
+        f.write("}")
     f.close()
 
 if __name__ == "__main__":
     working_dir = os.path.dirname(__file__)
+    graph_file_path = os.path.join(working_dir, '../Visuals/graph.dot')
     json_tokens_file_path = os.path.join(working_dir, "../Data/tokens.json")
     main(tokens_file_path=json_tokens_file_path)
